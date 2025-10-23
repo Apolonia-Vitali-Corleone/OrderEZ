95133c75100e4f01969d85cdf5caf6fda0b66fab
Apolonia-Vitali-Corleone <153711525+Apolonia-Vitali-Corleone@users.noreply.github.com>
2025-10-23 13:20:11 +0800

    Fix sql file

--- init.sql
+++ init.sql
@@ -1,42 +1,42 @@
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `order_ez` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+create database if not exists order_ez default character set utf8mb4 collate utf8mb4_unicode_ci default encryption = 'N';
 
-USE `order_ez`;
+use order_ez;
 
-DROP TABLE IF EXISTS `oe_order`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `oe_order` (
-  `order_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  `total_price` int NOT NULL,
-  PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+drop table if exists oe_order;
+create table oe_order
+(
+    order_id    bigint not null,
+    user_id     bigint not null,
+    total_price int    not null,
+    primary key (order_id)
+) engine = innodb
+  default charset = utf8mb4
+  collate = utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `oe_order_item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `oe_order_item` (
-  `order_item_id` bigint NOT NULL,
-  `order_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
-  `item_id` bigint NOT NULL,
-  `item_name` varchar(255) NOT NULL,
-  `item_price` int NOT NULL,
-  `item_count` int NOT NULL,
-  PRIMARY KEY (`order_item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+drop table if exists oe_order_item;
+create table oe_order_item
+(
+    order_item_id bigint       not null,
+    order_id      bigint       not null,
+    user_id       bigint       not null,
+    item_id       bigint       not null,
+    item_name     varchar(255) not null,
+    item_price    int          not null,
+    item_count    int          not null,
+    primary key (order_item_id)
+) engine = innodb
+  default charset = utf8mb4
+  collate = utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `oe_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `oe_user` (
-  `user_id` bigint NOT NULL AUTO_INCREMENT,
-  `username` varchar(64) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+drop table if exists oe_user;
+create table oe_user
+(
+    user_id  bigint       not null auto_increment,
+    username varchar(64)  not null,
+    password varchar(255) not null,
+    primary key (user_id),
+    unique key username (username)
+) engine = innodb
+  auto_increment = 31
+  default charset = utf8mb4
+  collate = utf8mb4_0900_ai_ci;
